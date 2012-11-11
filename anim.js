9b38d364a175b6a445fa46afd0af6c0535ce16fa
Graham S <smit4580@umn.edu>
2012-11-11 14:13:59 -0600

    better goal kick and goal animations, text doesn't cut out

--- anim.js
+++ anim.js
@@ -58,7 +58,7 @@ goalAnim.prototype.update = function() {
 }
 
 goalAnim.prototype.render = function( context ) {
-	if ( this.stage < 3 ) {
+	if ( this.stage < 3 || this.wipeY > -canvasDiagonal ) {
 		context.font = this.style;
 		context.strokeStyle = 'white';
 		context.lineWidth = 15;
@@ -87,37 +87,105 @@ goalAnim.prototype.loop = function() {
 function goalKickAnim() {
 	this.width = 72 * 4;
 	this.height = 72 + 20;
+
+	this.middle = canvas.height / 2 + this.height / 2;
+	this.bottom = canvas.height + 120;
 	
 	this.posX = canvas.width / 2;
-	this.posY = canvas.height + 120;
+	this.posY = this.bottom;
 	
 	this.string = "GOAL KICK";
 
 	this.velX = 0;
 	this.velY = -50;
 	
+	this.wipeVel = -25;
+	
 	this.style = "italic 120px bolder fantasy";
 
 	this.removeThis = false;
+	
+	this.stage = 0;
+	
+	this.wipes = 0;
+	
+	this.bulge = 0.0;
+	
+	this.wipeY = 0;
+	this.wipeYVel = -130;
+	this.stripeWidth = 40;
+	this.wipeStripes = canvasDiagonal / this.stripeWidth + 2;
 }
 
 goalKickAnim.prototype.complete = function() {
-	this.removeThis = true;
+	this.stage = 3;
 }
-
+ 
 goalKickAnim.prototype.update = function() {
-	this.posY += this.velY;
+	this.bulge *= 0.7;	
+	
+	switch( this.stage ) {
+		case 0:
+			this.posY += this.velY;
+			if ( this.posY < this.middle ) {
+				this.stage = 1;
+				this.bulge = 0.4;
+				this.wipes++;
+				this.posY = this.bottom;
+			}
+			break;
+		case 1:
+			this.posY += this.velY;
+			if ( this.posY < this.middle ) {
+				this.bulge = 0.4;
+				this.wipes++;
+				this.posY = this.bottom;
+				if ( this.wipes > 4 ) this.stage = 2;
+			}
+			break;
+		case 2:
+			this.wipeY += this.wipeYVel;
+			while ( this.wipeY < -canvasDiagonal - this.stripeWidth * 2 ) this.wipeY += this.stripeWidth * 2;
+			break;
+		case 3:
+			this.wipeY += this.wipeYVel;
+			if ( this.wipeY < -canvasDiagonal * 2 ) this.removeThis = true;
+			break;
+	}		
 }
 
 goalKickAnim.prototype.render = function( context ) {
 	context.font = this.style;
+	context.lineWidth = 15;	
+	context.fillStyle = 'blue';	
 	context.strokeStyle = 'white';
-	context.lineWidth = 15;
-	context.strokeText( this.string, this.posX, this.posY );
-	context.fillStyle = 'blue';
-	context.fillText( this.string, this.posX, this.posY );
-
-	context.fillRect( 0, this.posY + 60, canvas.width, canvas.height * 3);
+	
+	if ( this.stage > 0 && ( this.stage < 3 || this.wipeY > -canvasDiagonal)) {
+		context.save();
+			context.translate( this.posX, this.middle );
+			context.scale( 1.0 + this.bulge, 1.0 + this.bulge );
+			context.strokeText( this.string, 0, 0 );
+			context.fillText( this.string, 0, 0 );
+		context.restore();
+	}
+	if ( this.stage < 2 ) {
+		context.strokeText( this.string, this.posX, this.posY );
+		context.fillText( this.string, this.posX, this.posY );
+	} else {
+		if ( this.stage >= 2 ) {
+			context.save();
+				context.translate( 0, canvas.height );
+				context.rotate( Math.PI / 4 );
+				context.translate( -canvasDiagonal, 0);
+				for ( var y = 0; y < this.wipeStripes; y++ ) {
+					if ( y % 2 ) context.fillStyle = 'blue';
+				else context.fillStyle = 'white';
+				
+					context.fillRect( 0, this.wipeY + y * this.stripeWidth, canvasDiagonal * 2, this.stripeWidth);
+				}
+			context.restore();
+		}
+	}
 }
 
 goalKickAnim.prototype.loop = function() {
