5d6e06c096a1b762d96f6615abdc85ac911e02db
graham <graham@localhost.localdomain>
2015-08-06 15:16:53 -0500

    file describing different teams

---
+++ nations.js
@@ -0,0 +1,23 @@
+if ( typeof define !== "function" ) {
+	var define = require("amdefine")(module);
+}
+
+define( [], function() {
+
+var nations = [
+	pru = { name: 'Dnieper Cossacks', shortname: "DNI", img: 'img/cossacks50.png' },
+	cos = { name: 'Kingdom of Prussia', shortname: "PRU", img: 'img/prussia50.png' },
+	pol = { name: 'Polish Home Army', shortname: "POL", img: 'img/poland50.png' },
+	swe = { name: 'Swedish Empire', shortname: "SWE", img: 'img/sweden50.png' },
+];
+
+// Legio I Germanica
+// Mongols
+// Afrika Corps
+// Teutonic Knights
+// Immortals Persepolis
+
+
+return nations;
+
+})
